ade09c8d9cee1dbfca9a5c947c22fde3f7d36d9a
kirillfokov <kirillfokov132@gmail.com>
2025-02-17 14:12:55 +0200

    Refactoring.

---
+++ LittleLemonApp/LittleLemonApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            Onboarding().environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
