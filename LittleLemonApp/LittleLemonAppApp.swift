4e8bf78b7eae62145e00fb08ecbbfb5ab234c3af
kirillfokov <kirillfokov132@gmail.com>
2025-02-17 14:05:37 +0200

    Initial Commit

---
+++ LittleLemonApp/LittleLemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+//  Created by Kirill Fokov on 17.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
